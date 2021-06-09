a622b379678be9e121585d065238a4c1681619cd
simon71701 <54042406+simon71701@users.noreply.github.com>
2021-06-09 14:34:47 -0400

    Add files via upload

---
+++ Working/train.py
@@ -0,0 +1,230 @@
+import argparse
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from sklearn import metrics
+from utils import *
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+class Net(nn.Module):
+    def __init__(self, maxhits, args):
+        super(Net, self).__init__()
+        
+        self.batch_size = 3*args.maxhits
+        self.num_funnel_layers=args.num_funnel_layers
+        self.funnel_rate = (self.batch_size-3)/(self.num_funnel_layers+1)
+        self.dropout=args.dropout
+        
+        self.funnel_layers = nn.ModuleList()
+        self.funnel_layers.append(nn.BatchNorm1d(self.batch_size))
+        
+        for i in range(self.num_funnel_layers):
+            self.funnel_layers.append(
+                
+            nn.Sequential( 
+                nn.Linear(int(self.batch_size-self.funnel_rate*i), int(self.batch_size-self.funnel_rate*(i+1))),
+                nn.ReLU(),
+                nn.Dropout(self.dropout)
+                )
+                
+            )
+        
+        self.funnel_layers.append(
+            
+            nn.Sequential(
+                nn.Linear(int(self.batch_size-self.funnel_rate*(self.num_funnel_layers)), 3),
+                nn.ReLU(),
+                nn.Dropout(self.dropout)
+                )
+            
+            )
+        
+        self.output_layer = nn.Sequential( 
+            nn.Linear(3, 1),
+            nn.Sigmoid()
+            )
+        
+    def forward(self, x):
+        for layer in self.funnel_layers:
+            x = layer(x)
+            
+        x = self.output_layer(x)
+        return x
+
+def train_one_batch(classifier, optimizer, train_data, labels):
+    torch.enable_grad()
+    loss = FocalLoss(.5, 3)
+    optimizer.zero_grad()
+    
+    prediction = torch.flatten(classifier(train_data))
+    
+    error = loss(prediction, labels)
+    error.backward()
+    
+    optimizer.step()
+    
+    return error, prediction
+
+def test(classifier, test_data_loader, epoch, path, args, early_stop=False):
+    
+    classifier.eval()
+    loss = FocalLoss(.5, 3)
+    errors = []
+    
+    max_fpr = .001
+    
+    for batch, labels in test_data_loader:
+        with torch.no_grad():
+            prediction = torch.flatten(classifier(batch))
+            error = loss(prediction, labels)
+            fpr, tpr, thresholds = metrics.roc_curve(labels, prediction)
+            roc_auc = metrics.auc(fpr, tpr)
+            partial_roc_auc = metrics.roc_auc_score(labels, prediction, max_fpr=max_fpr)
+    
+    index = get_idx_for_interested_fpr(fpr, .01)
+    interested_tpr = float(tpr[index])
+    
+    if early_stop == True:
+        lw = 2
+        plt.clf()
+        plt.plot(fpr, tpr, color='darkorange',
+                lw=lw, label='ROC curve (area = %0.4f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.plot(max_fpr, interested_tpr, 'ro', label = '.001 Fpr, %.2f Recall (area = %.4f)' % (interested_tpr, partial_roc_auc))
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC Curve'.format(epoch))
+        plt.legend(loc="lower right")
+        plt.grid()
+        plt.savefig("{0}/Best_ROC_Curve.png".format(path))            
+        plt.close()
+        
+    elif epoch%10 == 0:
+        lw = 2
+        plt.clf()
+        plt.plot(fpr, tpr, color='darkorange',
+                lw=lw, label='ROC curve (area = %0.4f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.plot(max_fpr, interested_tpr, 'ro', label = '.001 Fpr, %.2f Recall (area = %.4f)' % (interested_tpr, partial_roc_auc))
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC Curve: Epoch #{0}'.format(epoch))
+        plt.legend(loc="lower right")
+        plt.grid()
+        plt.savefig("{0}/ROC_Curve_Epoch{1}.png".format(path, epoch))            
+        plt.close()
+        
+    return error, roc_auc
+
+def trainModel(train_data, test_data, maxhits, device, args):
+    
+    path = "TrainingLogs/"
+    
+    if args.extra_filter:
+        path += "Extra_Filter/"
+    else:
+        path += "No_Extra_Filter/"
+    
+    if args.mix:
+        path += "{0}Mix/".format(int(args.mix*100))
+    else:
+        path += "No_Mix/pu{0}/".format(args.pileup)
+        
+    path += "{0}Layers_{1}Dropout".format(args.num_funnel_layers, args.dropout)
+    
+    if args.early_stop:
+        early_stopping = EarlyStopping(path="{0}/checkpoint.pt".format(path), patience=10)
+    
+    auc_score = .50
+    running = True
+    retries = 0
+    
+    
+    while auc_score == .50 and running == True:
+        classifier = Net(maxhits, args)
+        classifier.to(device)
+        opt = torch.optim.Adam(classifier.parameters(),lr=args.lr)
+        
+        total_params = sum(p.numel() for p in classifier.parameters() if p.requires_grad)
+        print(total_params)
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        
+        data_loader = torch.utils.data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True, drop_last=True)
+        test_data_loader = torch.utils.data.DataLoader(test_data, batch_size=len(test_data), shuffle=True, drop_last=True)
+
+        errors = []
+        test_errors = []
+        
+        for epoch in tqdm(range(1, args.epochs+1)):
+            for batch, labels in data_loader:
+                error, prediction = train_one_batch(classifier, opt, batch, labels)
+
+            errors.append(float(error))
+            if args.early_stop:
+                test_error, auc_score = test(classifier, test_data_loader, epoch, path, args, early_stop=early_stopping.early_stop)
+            else:
+                test_error, auc_score = test(classifier, test_data_loader, epoch, path, args)
+            
+            if epoch== 1 and auc_score == .50:
+                print("Retrying")
+                retries += 1
+                del classifier
+                del opt
+                del data_loader
+                del test_data_loader
+                break
+        
+            test_errors.append(float(test_error))
+            
+            if args.early_stop:
+                early_stopping(test_error, classifier)
+            
+                if early_stopping.early_stop:
+                    print("Early stopping")
+                    test_error = test(classifier, test_data_loader, epoch-10, path, args, early_stop=early_stopping.early_stop)
+                    classifier.load_state_dict(torch.load('{0}/checkpoint.pt'.format(path)))
+                    torch.save(classifier.state_dict(), "{0}/BestClassifierSettings".format(path))
+                    
+                    stopping_point = epoch-10
+                    
+                    break
+            
+            if epoch%10 == 0:
+                print("Epoch {0} Finished: Current Total Training Error={1} ".format(epoch+1, error))
+                
+                plt.clf()                                   
+                plt.plot(range(0,len(list(errors))), list(errors))
+                plt.xlabel("Epoch")
+                plt.title("Training Error: FL={0}".format(args.num_funnel_layers))
+                plt.ylabel("Total Training Error")
+                plt.text(.95, .95, "{0} Trainable Parameters".format(total_params), bbox=props)
+                plt.grid()
+                plt.savefig("{0}/FunnelErrorCurve_Epoch={1}.png".format(path, epoch))
+                plt.clf()
+                
+                if epoch != 0:
+                    test_epochs = [i for i in range(len(test_errors))]
+                    plt.plot(test_epochs, list(test_errors))
+                    plt.xlabel("Epoch")
+                    plt.title("Testing Error: FL={0}".format(args.num_funnel_layers))
+                    plt.ylabel("Total Test Error")
+                    plt.grid()
+                    plt.savefig("{0}/FunnelErrorCurve_Test_Epoch={1}.png".format(path, epoch))
+                    plt.clf()
+                
+                torch.save(classifier.state_dict(), "{0}/Epoch{1}_ClassifierSettings".format(path, epoch))
+            
+            if epoch == args.epochs:
+                running == False
+        
+            
+    
---
+++ Working/utils.py
@@ -0,0 +1,263 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+import argparse
+import numpy as np
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description='GNN arguments.')
+
+    parser.add_argument('--batch_size', type=int,
+                        help='Training batch size')
+    parser.add_argument('--dropout', type=float,
+                        help='Dropout rate')
+    parser.add_argument('--epochs', type=int,
+                        help='Number of training epochs')
+    parser.add_argument('--lr', type=float,
+                       help='Learning Rate')
+    parser.add_argument('--pileup', type=int,
+                       help='Pileup amount. Options: 0, 200')
+    parser.add_argument('--num_funnel_layers', type=int,
+                       help='Number of Funneling Layers')
+    parser.add_argument('--maxhits', type=int,
+                        help="Maximum number of hits allowed")
+    parser.add_argument('--extra_filter', type=int,
+                        help="Filter for pt and eta. Options: 1 for true, 0 for false")
+    parser.add_argument('--mix', type=float,
+                        help='If >0, denotes the fraction with which the samples are mixed. See documentation of the function mixData for more details.')
+    parser.add_argument('--early_stop', type=int,
+                        help="Denotes whether training will use early stopping. 1 for true, 0 for false")
+
+
+    parser.set_defaults(batch_size=50,
+                        dropout=.1,
+                        epochs=250,
+                        lr=.0001,
+                        pileup=0,
+                        num_funnel_layers=5,
+                        maxhits=71,
+                        extra_filter=0,
+                        mix=0,
+                        early_stop=0)
+
+    return parser.parse_args()
+
+class FocalLoss(torch.nn.Module):
+    def __init__(self, alpha: float, gamma: float, reduction: str = "mean"):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+    def forward(self, inputs, targets):
+        ce_loss = F.binary_cross_entropy(inputs, targets, reduction="none")
+        p_t = inputs * targets + (1 - inputs) * (1 - targets)
+        loss = ce_loss * ((1 - p_t) ** self.gamma)
+        if self.alpha >= 0:
+            alpha_t = self.alpha * targets + (1 - self.alpha) * (1 - targets)
+            loss = alpha_t * loss
+        if self.reduction == "mean":
+            loss = loss.mean()
+        elif self.reduction == "sum":
+            loss = loss.sum()
+        return loss
+
+# From pytorchtools
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement. 
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+            trace_func (function): trace print function.
+                            Default: print            
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
+        
+    def __call__(self, val_loss, model):
+
+        score = (-1)*val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
+
+def make_lookup(variables):
+    lookup = dict({})
+    for i in range(len(variables)):
+        lookup.update({variables[i]:i})
+        
+    return lookup
+
+## Filter for events that only have 1 tau
+def filterPU200(dataset):
+    singledOut = []
+    
+    for event in dataset:
+        if event[0] == 1:
+            singledOut.append(event)
+    
+    return singledOut
+
+## Return a filtered dataset that only has the specified characteristic
+def collectChar(index, dataset):
+    char = []
+    
+    for i in range(len(dataset)):
+        char.append(dataset[i][index])
+    
+    
+    return char
+
+# Filter for events that have up to a specified number of muons and pad events with zeros.  Ordered (Phi, Eta, R) 
+def filterandpad(dataset, maxhits, variables, interested_vars, pt_eta_filter):
+    filtered = []
+    
+    lookup = make_lookup(variables)
+    indices = []
+    
+    indices = [lookup[var] for var in interested_vars]
+    criteria_indices = [lookup[var] for var in ['n_mu_hit', 'gen_mu_pt', 'gen_mu_eta']]
+    
+    for event in dataset:
+        chars = []
+        if event[criteria_indices[0]] <= maxhits:
+            
+            if pt_eta_filter:
+                if (min(event[criteria_indices[1]]) > .5) and ((min(abs(event[criteria_indices[2]])) > 1.2) and (max(abs(event[criteria_indices[2]])) < 2.8)):
+                    for i in indices:
+                        char = np.pad(event[i], (0,maxhits-len(event[i])), 'constant', constant_values=0)
+                        for muon in char:
+                            chars.append(muon)
+                  
+                    filtered.append(chars)          
+            else:
+                for i in indices:
+                    char = np.pad(event[i], (0,maxhits-len(event[i])), 'constant', constant_values=0)
+                    for muon in char:
+                        chars.append(muon)
+                            
+                filtered.append(chars)
+        
+        
+        
+    return filtered
+
+# See filterandpad
+def minbias_filterandpad(minbias, maxhits, minbiasvars, interested_vars):
+    filtered = []
+    
+    lookup = make_lookup(minbiasvars)
+    indices = []
+    
+    indices = [lookup[var] for var in interested_vars]
+    hits_index = lookup['n_mu_hit']
+    
+    for event in minbias:
+        chars = []
+        if event[hits_index] <= maxhits:
+            for i in indices:
+                char = np.pad(event[i], (0,maxhits-len(event[i])), 'constant', constant_values=0)
+                for muon in char:
+                    chars.append(muon)
+                
+            filtered.append(chars)
+    
+    return filtered 
+
+def mixData(datasets, fraction):
+    
+    """
+    Args:   datasets (tuple): Tuple of length 2 containing the datasets to be mixed
+            fraction (float): Fraction denoting how the samples are mixed.
+                              Example: If fraction is .8, the datasets will be mixed such that the mixed dataset consists of 80% of the first dataset
+                              and 20% of the second dataset.
+    
+    """
+    
+    mixed = []
+    
+    lengths = []
+    
+    for i in range(len(datasets)):
+        lengths.append(len(datasets[i]))
+    
+    for i in range(len(lengths)):
+        if lengths[i] == min(lengths):
+            minlength = lengths[i]
+    
+    final_length = int(minlength/fraction)
+    
+    for i in range(minlength):
+        mixed.append(datasets[0][i])
+    
+    for i in range(final_length-minlength):
+        mixed.append(datasets[1][i])
+    
+        
+    return mixed
+
+# By Siqi Miao
+def get_idx_for_interested_fpr(fpr, interested_fpr):
+    res = []
+    for i in range(1, fpr.shape[0]):
+        if fpr[i] > interested_fpr:
+            res.append(i-1)
+            break
+    return res
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
