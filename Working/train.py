ccaed43290c31daff2ed9460f82a361f08bdb608
simon71701 <54042406+simon71701@users.noreply.github.com>
2021-08-25 12:53:09 -0500

    Update train.py

--- Working/train.py
+++ Working/train.py
@@ -2,9 +2,9 @@ import argparse
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 from sklearn import metrics
-from utils import *
+from utils2 import *
+from test_model1_4 import *
 
 import torch
 import torchvision
@@ -12,67 +12,61 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-
-class Net(nn.Module):
-    def __init__(self, maxhits, args):
-        super(Net, self).__init__()
-        
-        self.batch_size = 3*args.maxhits
-        self.num_funnel_layers=args.num_funnel_layers
-        self.funnel_rate = (self.batch_size-3)/(self.num_funnel_layers+1)
-        self.dropout=args.dropout
-        
-        self.funnel_layers = nn.ModuleList()
-        self.funnel_layers.append(nn.BatchNorm1d(self.batch_size))
-        
-        for i in range(self.num_funnel_layers):
-            self.funnel_layers.append(
-                
-            nn.Sequential( 
-                nn.Linear(int(self.batch_size-self.funnel_rate*i), int(self.batch_size-self.funnel_rate*(i+1))),
-                nn.ReLU(),
-                nn.Dropout(self.dropout)
-                )
-                
-            )
-        
-        self.funnel_layers.append(
-            
-            nn.Sequential(
-                nn.Linear(int(self.batch_size-self.funnel_rate*(self.num_funnel_layers)), 3),
-                nn.ReLU(),
-                nn.Dropout(self.dropout)
-                )
-            
-            )
-        
-        self.output_layer = nn.Sequential( 
-            nn.Linear(3, 1),
-            nn.Sigmoid()
-            )
-        
-    def forward(self, x):
-        for layer in self.funnel_layers:
-            x = layer(x)
-            
-        x = self.output_layer(x)
-        return x
+from torch.utils.tensorboard.summary import hparams as hp
 
 def train_one_batch(classifier, optimizer, train_data, labels):
+    torch.set_default_dtype(torch.float32)
     torch.enable_grad()
     loss = FocalLoss(.5, 3)
     optimizer.zero_grad()
     
-    prediction = torch.flatten(classifier(train_data))
-    
+    prediction = torch.flatten(classifier(train_data)).float()
+    labels.float()
     error = loss(prediction, labels)
     error.backward()
     
     optimizer.step()
     
     return error, prediction
+    
+def trainAUC(classifier, auc_loader, path, epoch):
+    classifier.eval()
+    
+    max_fpr = .001
+    
+    R_LHC = 2760*11.246 ##kHz
+    
+    for batch, labels in auc_loader:
+        with torch.no_grad():
+            prediction = torch.flatten(classifier(batch))
+            fpr, tpr, thresholds = metrics.roc_curve(labels, prediction)
+            roc_auc = metrics.auc(fpr, tpr)
+            partial_roc_auc = metrics.roc_auc_score(labels, prediction, max_fpr=max_fpr)
+    
+    if epoch%10 == 0:
+        
+        index = get_idx_for_interested_fpr(fpr, max_fpr)
+        interested_tpr = float(tpr[index])
+    
+        lw = 2
+        plt.clf()
+        plt.plot(fpr*R_LHC, tpr, color='darkorange',
+                lw=lw, label='ROC curve (area = %0.4f)' % roc_auc)
+        plt.ylim([0.0, 1.05])
+        plt.xlim((0, 100))
+        plt.axvline(x = 30, label="30 kHz", linestyle='dashed')
+        #plt.plot(max_fpr*R_LHC, interested_tpr, 'ro', label = '.001 Fpr, %.2f Recall (area = %.4f)' % (interested_tpr, partial_roc_auc))
+        plt.xlabel('Trigger Rate (kHz)')
+        plt.ylabel('Trigger Acceptance')
+        plt.title('Epoch #{0} Training ROC Curve'.format(epoch))
+        plt.legend(loc="lower right")
+        plt.grid()
+        plt.savefig("{0}/Training_ROC_Curve_Epoch{1}.png".format(path, epoch))            
+        plt.close()
+    
+    return roc_auc
 
-def test(classifier, test_data_loader, epoch, path, args, early_stop=False):
+def validate(classifier, valid_data_loader, epoch, path, args, early_stop=False):
     
     classifier.eval()
     loss = FocalLoss(.5, 3)
@@ -82,7 +76,7 @@ def test(classifier, test_data_loader, epoch, path, args, early_stop=False):
     
     max_fpr = .001
     
-    for batch, labels in test_data_loader:
+    for batch, labels in valid_data_loader:
         with torch.no_grad():
             prediction = torch.flatten(classifier(batch))
             error = loss(prediction, labels)
@@ -90,55 +84,58 @@ def test(classifier, test_data_loader, epoch, path, args, early_stop=False):
             roc_auc = metrics.auc(fpr, tpr)
             partial_roc_auc = metrics.roc_auc_score(labels, prediction, max_fpr=max_fpr)
     
-    index = get_idx_for_interested_fpr(fpr, .01)
-    interested_tpr = float(tpr[index])
-    
+    R_LHC = 2760*11.246
     if early_stop == True:
+        index = get_idx_for_interested_fpr(fpr, max_fpr)
+        interested_tpr = float(tpr[index])   
+        fpr = fpr*R_LHC
         lw = 2
         plt.clf()
         plt.plot(fpr, tpr, color='darkorange',
                 lw=lw, label='ROC curve (area = %0.4f)' % roc_auc)
-        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-        plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
-        plt.plot(max_fpr, interested_tpr, 'ro', label = '.001 Fpr, %.2f Recall (area = %.4f)' % (interested_tpr, partial_roc_auc))
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
+        plt.xlim((0, 100))
+        #plt.plot(, interested_tpr, 'ro', label = '.001 Fpr, %.2f Recall (area = %.4f)' % (interested_tpr, partial_roc_auc))
+        plt.xlabel('Trigger Rate (kHz)')
+        plt.ylabel('Trigger Acceptance')
         plt.title('ROC Curve: {0} Trainable Parameters'.format(total_params))
         plt.legend(loc="lower right")
         plt.grid()
-        plt.savefig("{0}/Best_ROC_Curve.png".format(path))            
+        plt.savefig("{0}/Best_Validation_ROC_Curve.png".format(path))            
         plt.close()
         
     elif epoch%10 == 0:
+        index = get_idx_for_interested_fpr(fpr, max_fpr)
+        interested_tpr = float(tpr[index])  
+        fpr = fpr*R_LHC
         lw = 2
         plt.clf()
         plt.plot(fpr, tpr, color='darkorange',
                 lw=lw, label='ROC curve (area = %0.4f)' % roc_auc)
-        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-        plt.xlim([0.0, 1.0])
+        plt.xlim((0, 100))
         plt.ylim([0.0, 1.05])
-        plt.plot(max_fpr, interested_tpr, 'ro', label = '.001 Fpr, %.2f Recall (area = %.4f)' % (interested_tpr, partial_roc_auc))
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
+        plt.axvline(x = 30, label="30 kHz", linestyle='dashed')
+        #plt.plot(max_fpr, interested_tpr, 'ro', label = '.001 Fpr, %.2f Recall (area = %.4f)' % (interested_tpr, partial_roc_auc))
+        plt.xlabel('Trigger Rate (kHz)')
+        plt.ylabel('Trigger Acceptance')
         plt.title('Epoch #{0} ROC Curve: {1} Trainable Parameters'.format(epoch, total_params))
         plt.legend(loc="lower right")
         plt.grid()
-        plt.savefig("{0}/ROC_Curve_Epoch{1}.png".format(path, epoch))            
+        plt.savefig("{0}/Validation_ROC_Curve_Epoch{1}.png".format(path, epoch))            
         plt.close()
         
     return error, roc_auc
 
-def trainModel(train_data, test_data, maxhits, device, args):
+def trainModel(train_data, valid_data, test_data, maxhits, device, args):
     
-    auc_score = .50
+    valid_auc_score = .50
     running = True
     retries = 0
     
     
-    while auc_score == .50 and running == True:
+    while valid_auc_score == .50 and running == True:
         classifier = Net(maxhits, args)
-        classifier.to(device)
+        #classifier.to(device)
         opt = torch.optim.Adam(classifier.parameters(),lr=args.lr)
         
         if args.extra_filter == 1:
@@ -153,11 +150,10 @@ def trainModel(train_data, test_data, maxhits, device, args):
         
         
         data_loader = torch.utils.data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True, drop_last=True)
+        valid_data_loader = torch.utils.data.DataLoader(valid_data, batch_size=len(valid_data), shuffle=True, drop_last=True)
         test_data_loader = torch.utils.data.DataLoader(test_data, batch_size=len(test_data), shuffle=True, drop_last=True)
         
         events, labels = next(iter(data_loader))
-        grid = torchvision.utils.make_grid(events)
-        tb.add_graph(classifier, events)
         
         for epoch in tqdm(range(1, args.epochs+1)):
             errors = []
@@ -166,22 +162,26 @@ def trainModel(train_data, test_data, maxhits, device, args):
                 errors.append(float(error))
             
             if args.early_stop:
-                test_error, auc_score = test(classifier, test_data_loader, epoch, tb.log_dir, args, early_stop=early_stopping.early_stop)
+                valid_error, valid_auc_score = validate(classifier, valid_data_loader, epoch, tb.log_dir, args, early_stop=early_stopping.early_stop)
             else:
-                test_error, auc_score = test(classifier, test_data_loader, epoch, tb.log_dir, args)
+                valid_error, valid_auc_score = validate(classifier, valid_data_loader, epoch, tb.log_dir, args)
+                test_error, test_auc_score = testModel(classifier, test_data_loader, epoch, tb.log_dir, args)
             
-            if epoch== 1 and auc_score == .50:
+            if epoch== 1 and valid_auc_score == .50:
                 print("Retrying")
                 retries += 1
                 del classifier
                 del opt
                 del data_loader
+                del valid_data_loader
                 del test_data_loader
                 break
         
             tb.add_scalar("Training Loss", np.mean(errors), epoch)
-            tb.add_scalar("Validation Loss", float(test_error), epoch)
-            tb.add_scalar("Validation AUROC", float(auc_score), epoch)
+            tb.add_scalar("Validation Loss", float(valid_error), epoch)
+            tb.add_scalar("Validation AUROC", float(valid_auc_score), epoch)
+            tb.add_scalar("Testing Loss", float(test_error), epoch)
+            tb.add_scalar("Testing AUROC", float(test_auc_score), epoch)
             
             tb.add_histogram('funnel_layers.1.0.weight', classifier.funnel_layers[1][0].weight, epoch)
             tb.add_histogram('funnel_layers.1.0.bias', classifier.funnel_layers[1][0].bias, epoch)
@@ -192,11 +192,11 @@ def trainModel(train_data, test_data, maxhits, device, args):
             )
             
             if args.early_stop:
-                early_stopping(test_error, classifier)
+                early_stopping(valid_error, classifier)
             
                 if early_stopping.early_stop:
                     print("Early stopping")
-                    test_error = test(classifier, test_data_loader, epoch-10, tb.log_dir, args, early_stop=early_stopping.early_stop)
+                    valid_error = validatet(classifier, valid_data_loader, epoch-10, tb.log_dir, args, early_stop=early_stopping.early_stop)
                     classifier.load_state_dict(torch.load('{0}/checkpoint.pt'.format(tb.log_dir)))
                     torch.save(classifier.state_dict(), "{0}/BestClassifierSettings".format(tb.log_dir))
                     
@@ -208,10 +208,9 @@ def trainModel(train_data, test_data, maxhits, device, args):
                 torch.save(classifier.state_dict(), "{0}/Epoch{1}_ClassifierSettings".format(tb.log_dir, epoch))
             
             if epoch == args.epochs:
-                running == False
+                running = False
                 torch.save(classifier.state_dict(), "{0}/FinalClassifierSettings".format(tb.log_dir, epoch))
+                testModel(classifier, test_data_loader, epoch, tb.log_dir, args, running=False)
                 
                 tb.close()
-    
-    return tb.log_dir
     
